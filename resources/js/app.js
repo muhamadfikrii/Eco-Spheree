f0b9a3a72be6f6ce8b66efadecf52cbda13dff72
Muhamad Fikri <muhamadfikriardiansyah95@gmail.com>
2025-11-14 15:02:59 +0700

    delete environmental js,update navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,6 @@ import './gsap';
 import 'animate.css';
 import Alpine from 'alpinejs';
 
-// Impor data dari file terpisah
-import { orbitingNodes, modernEcosphere } from './dataEnvironmental';
-
 // Buat Alpine.js tersedia secara global
 window.Alpine = Alpine;
-
-// Buat data `orbitingNodes` tersedia secara global agar bisa diakses oleh fungsi modernEcosphere
-window.orbitingNodes = orbitingNodes;
-
-// Daftarkan komponen Alpine.js
-Alpine.data('modernEcosphere', modernEcosphere);
-
-// Mulai Alpine.js
 Alpine.start();
