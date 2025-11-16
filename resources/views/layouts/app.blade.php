63abfaaaca7459af4428d191986432700a50c050
Muhamad Fikri <muhamadfikriardiansyah95@gmail.com>
2025-11-16 13:29:24 +0700

    inisialisasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Eco-Track') }}</title>
+  <title>{{ config('app.name', 'Eco-Spheree') }}</title>
 
   <!-- Fonts -->
   <link href="https://fonts.bunny.net/css?family=inter:400,600&display=swap" rel="stylesheet" />
@@ -20,7 +20,8 @@
   <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css" />
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  {{-- <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"> --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
 
   <!-- Particle Js -->
   <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
